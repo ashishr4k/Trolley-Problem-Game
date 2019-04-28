d87e735a90179d7d0a0adbee092806ec8632c27b
ashishr4k <ashishr4k@github.com>
2019-04-28 15:08:08 +1000

    Switch and scenario
    Start on switch script for changing tracks
    Start on scenario script for holding scenario information

---
+++ Trolley Problem/Assets/Scripts/Scenario.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scenario : MonoBehaviour
+{
+	public int outcomes;
+	public int curr_out = 1;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	void ScenearioEnd(){
+		//code to write to database and display stats from database
+	}
+}
