dcef10ca491565455d8b2847e9ad507a14cf2e9b
ReXLaC <38935439+ReXLaC@users.noreply.github.com>
2019-05-07 00:39:52 +1000

    Menu & Level select
    I have add menu & level select scenes in to the project folder

---
+++ Menu $ LevelSelect/Menu $ LevelSelect/Assets/Scripts/LoadScenes.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScenes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void SelectScene()
+    {
+        switch (this.gameObject.name)
+        {
+            case "LevelSelectButton":
+                SceneManager.LoadScene("Level Selection");
+                break;
+            case "QuickStartButton":
+                SceneManager.LoadScene("None");
+                break;
+            case "ResultsButton":
+                SceneManager.LoadScene("None");
+                break;
+        }
+    }
+}
