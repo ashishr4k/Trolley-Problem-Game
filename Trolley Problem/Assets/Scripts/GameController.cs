f9bb04cf209340901570f1be06ed4b10d6bac4e7
ReXLaC <38935439+ReXLaC@users.noreply.github.com>
2019-05-20 23:41:14 +1000

    Revert "Switch&checkbox"
    This reverts commit 815404f554976c47c97d5a7958d27517b030362d.

--- Trolley Problem/Assets/Scripts/GameController.cs
+++ Trolley Problem/Assets/Scripts/GameController.cs
@@ -5,57 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
-    private int numScenarios = 3;
+
     // Start is called before the first frame update
     void Start()
     {
-        if (PlayerPrefs.GetString("Layout") == "")
-        {
-            PlayerPrefs.SetString("Layout", "Train");
-        }
-        bool[] a = GetLevels();
-        if (PlayerPrefs.GetString("FirstTime") == "")
-        {
-            bool[] level = new bool[numScenarios];
-            for(int i  = 0; i < numScenarios; i++)
-            {
-                level[i] = false;
-            }
-            SetLevels(level);
-            Debug.Log("Set all levels");
-            PlayerPrefs.SetString("FirstTime", "No");
-        }
-    }
 
-    public static void SetLevels(bool[] levels)
-    {
-        PlayerPrefs.SetString("Level", GetSerializedString(levels));
-    }
-
-    // Use this to get integer array
-    public static bool[] GetLevels()
-    {
-        string[] data = PlayerPrefs.GetString("Level", "true").Split('|');
-        //Debug.Log(data.Length);
-        bool[] val = new bool[data.Length];
-        bool levelState;
-        for (int i = 0; i < val.Length; i++)
-        {
-            val[i] = bool.TryParse(data[i], out levelState) ? levelState : false;
-        }
-        return val;
-    }
-
-    private static string GetSerializedString(bool[] data)
-    {
-        if (data.Length == 0) return string.Empty;
-
-        string result = data[0].ToString();
-        for (int i = 1; i < data.Length; i++)
-        {
-            result += ("|" + data[i]);
-        }
-        return result;
     }
 
     // Update is called once per frame
--- Trolley Problem/Assets/Scripts/Scenario.cs
+++ Trolley Problem/Assets/Scripts/Scenario.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class Scenario : MonoBehaviour
 {
-    private static int id = 0;
+    private static int id;
 
     private int outcomes;        //number of choices/tracks
 	public int curr_out = 2;    //current track
@@ -17,30 +17,26 @@ public class Scenario : MonoBehaviour
     public Text EndScreenText;
     public GameObject Finish;
     public GameObject Next;
+    public float sce_time = 7f;
+    bool over = false;
+    public int people1;
+    public int people2;
+    public int people3;
+    public GameObject[] pTrack1;
+    public GameObject[] pTrack2;
+    public GameObject[] pTrack3;
 
-    int people1;
-    int people2;
-    int people3;
 
     public GameObject Track3;
     public GameObject Switch2;
 
-    const int totalScenarios = 3;
-    bool[] skip;    //true to skip
-
-    public GameObject char1;
-    public GameObject spawner1;
-    public GameObject spawner2;
-    public GameObject spawner3;
+    int totalScenarios = 1;
     // Start is called before the first frame update
     void Start()
     {
-        //levels to skip
-        skip = GetLevels();
-        id = SkipLevels(skip, id);
-
-        //Debug.Log("Number of Scenarios: "+totalScenarios);
+        Debug.Log("Number of Scenarios: "+totalScenarios);
         LoadScenarioData(id);
+        
 
         if (outcomes < 3)
         {
@@ -51,38 +47,32 @@ public class Scenario : MonoBehaviour
         m_Animator = train.GetComponent<Animator>();
         EndScreen.SetActive(false);
 
-        //people on tracks 
-        LoadPeople(people1, spawner1);
-        LoadPeople(people2, spawner2);
-        LoadPeople(people3, spawner3);
+        //people on tracks
+        generatePeople(people1, pTrack1);
+        generatePeople(people2, pTrack2);
+        generatePeople(people3, pTrack3);
     }
 
     // Update is called once per frame
     void Update()
     {
-
-    }
-
-    int SkipLevels(bool[] levelStates, int id)
-    {
-        for (int i = id; i < levelStates.Length; i++)
+        //Debug.Log((int)Time.timeSinceLevelLoad);
+        //Example call to end scenario if using timer instead of checking when train reaches end of track
+        if (Time.timeSinceLevelLoad > sce_time && !over)
         {
-            if (!levelStates[i])
-            {
-                //Debug.Log("next: "+ i);
-                return i;
-            }
+            ScenearioEnd();
         }
-        return levelStates.Length;
     }
 
-	public void ScenearioEnd(){
+	void ScenearioEnd(){
+        //code to write to database and display stats from database
 
+        
         //increment scenario id
         id++;
-        id = SkipLevels(skip, id);
+        over = true;
 
-        //Debug.Log("Choice Made: " + m_Animator.GetInteger("Track"));
+        Debug.Log("Choice Made: " + m_Animator.GetInteger("Choice"));
         //Debug.Log("Next Scene ID: " + id);
 
         //Next scenario or finish
@@ -102,26 +92,23 @@ public class Scenario : MonoBehaviour
         }
     }
 
-    void LoadPeople(int nPeople, GameObject startPos)
+    void generatePeople(int people, GameObject[] track)
     {
-        //some sort of switch statement to decide character prefab being instantiated
-
-        for (int i = 0; i < nPeople; i++)
+        for (int i = 0; i < people; i++)
         {
-            float offset = (float)i / 2;
-            Instantiate(char1, startPos.transform.position + (offset * Vector3.right), Quaternion.identity);
+            track[i].SetActive(true);
         }
     }
 
     //some hardcoded data for scenarios
     int[] numTracks = { 2, 3, 3 };
     int[] onTrack1 = { 1, 2, 1 };
-    int[] onTrack2 = { 2, 1, 1 };
+    int[] onTrack2 = { 3, 1, 1 };
     int[] onTrack3 = { 0, 2, 1 };
-    string[] infoTrack1 = { "An adult", "Two homeless adults","Your friend" };
-    string[] infoTrack2 = { "Two elderly men", "A homeless child", "Your Neighbour" };
-    string[] infoTrack3 = { "", "A wealthy couple", "Your boss" };
-    string[] endText = { "50 % picked the same choice", "50 % picked the same choice", "50 % picked the same choice" };
+    string[] infoTrack1 = { "Test", "Test3","Test5" };
+    string[] infoTrack2 = { "Test2", "Test4", "Test6" };
+    string[] infoTrack3 = { "", "Testb", "Testc" };
+    string[] endText = { "% Test", "% Test2", "% Test3" };
     void LoadScenarioData(int id)
     {
         outcomes = numTracks[id];
@@ -132,19 +119,5 @@ public class Scenario : MonoBehaviour
         choiceText[1].text = infoTrack2[id];
         choiceText[2].text = infoTrack3[id];
         EndScreenText.text = endText[id];
-    }
-
-
-    //code from https://answers.unity.com/questions/940020/playerprefsx-intarray.html for getting serialized array stored in playerprefs
-    public static bool[] GetLevels()
-    {
-        string[] data = PlayerPrefs.GetString("Level", "true").Split('|');
-        bool[] val = new bool[data.Length];
-        bool levelState;
-        for (int i = 0; i < val.Length; i++)
-        {
-            val[i] = bool.TryParse(data[i], out levelState) ? levelState : false;
-        }
-        return val;
     }
 }
