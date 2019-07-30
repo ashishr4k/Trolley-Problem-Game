8465544e185874b75997612e83a7cf3e99ee927b
ReXLaC <38935439+ReXLaC@users.noreply.github.com>
2019-07-30 14:54:52 +1000

    Tutorial signs
    I have add two signs to indicate player how to play the game.

---
+++ Trolley Problem/Assets/Scripts/DelayDestory.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayDestory : MonoBehaviour
+{
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(gameObject, 7f);  
+    }
+}
